997c2b679f309b40764202507e692a24f91932e4
karydesc <karydes.c@icloud.com>
2023-06-02 19:54:24 +0300

    progress

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-let main = document.querySelector(".main-container");
-
+let bcolor;
+let dcolor;
+const board=document.querySelector(".board");
+const colorselec=document.querySelector(".color");
+const dimselect=document.querySelector(".color");
